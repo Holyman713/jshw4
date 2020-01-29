bfce579efc8aed2e5fed705131889a50254152e6
Alex <alexwarchik@mail.ru>
2020-01-29 21:20:47 +0500

    v1

---
+++ js/script.js
@@ -0,0 +1,71 @@
+var name = prompt('Введите Имя')
+
+function year () {
+    var num1 = +prompt('Введите сегодняшний год');
+    var num2 = +prompt('Введите год рождения');
+
+    let year = num1 - num2;
+    return year;
+}
+console.log('Имя ' + name + ', ' + 'Ваш возраст ' + year());
+
+
+
+
+
+function sign(min1, max1) {
+    return Math.floor(Math.random() * (max1+1 - min1) + min1);
+}
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+var number = prompt ('Введите количество примеров')
+let userNum1 = +prompt ('Введите min число');
+let userNum2 = +prompt ('Введите max число');
+
+for (let i = 0; i<10 ; i++) {
+    let randomNum1 = random(userNum1, userNum2);
+    let randomNum2 = random(userNum1, userNum2);
+    let sign1 = sign(1,4);
+    if(sign1 == 1){
+        let userAnswer = +prompt(randomNum1 + ' + ' + randomNum2);
+        if ((randomNum1 + randomNum2) == userAnswer) {
+            console.log('Ваш ответ верный - ' + userAnswer);
+        }
+        else {
+            userAnswer = randomNum1 + randomNum2;
+            console.log('Ваш ответ неверный, правильный ответ ' + userAnswer);            
+        }    
+    }
+    else if(sign1 == 2){
+        let userAnswer = +prompt(randomNum1 + ' - ' + randomNum2);
+        if ((randomNum1 - randomNum2) == userAnswer) {
+            console.log('Ваш ответ верный - ' + userAnswer);
+        }
+        else {
+            userAnswer = randomNum1 - randomNum2;
+            console.log('Ваш ответ неверный, правильный ответ ' + userAnswer);            
+        }  
+    }
+    else if(sign1 == 3){
+        let userAnswer = +prompt(randomNum1 + ' / ' + randomNum2);
+        if ((randomNum1 / randomNum2) == userAnswer) {
+            console.log('Ваш ответ верный - ' + userAnswer);
+        }
+        else {
+            userAnswer = randomNum1 / randomNum2;
+            console.log('Ваш ответ неверный, правильный ответ ' + userAnswer);            
+        }  
+    }
+    else if(sign1 == 4){
+        let userAnswer = +prompt(randomNum1 + ' * ' + randomNum2);
+        if ((randomNum1 * randomNum2) == userAnswer) {
+            console.log('Ваш ответ верный - ' + userAnswer);
+        }
+        else {
+            userAnswer = randomNum1 * randomNum2;
+            console.log('Ваш ответ неверный, правильный ответ ' + userAnswer);            
+        }  
+    }   
+}
+
